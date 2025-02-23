f0b9a60c4e124491fd5c0bb31cd56b6329c60fdc
BANGKHEN\5-1001 <ictservice1@spu.ac.th>
2025-02-23 15:19:42 +0700

    JWT Token

--- src/api/registration/route.ts
+++ src/api/registration/route.ts
@@ -1,61 +1,95 @@
 import express, { Request, Response } from "express";
-import dayjs from 'dayjs'
- import 'dayjs/locale/zh-cn' // import locale
+import dayjs from "dayjs";
+import "dayjs/locale/zh-cn"; // import locale
+import jwt from "jsonwebtoken";
 import { user } from "../../mongDB/userSchema";
-import { auth } from "../../mongDB/authSchema"
+import { auth } from "../../mongDB/authSchema";
 
 export const tempAccount = express();
 
-tempAccount.post("/createUser", async (req: Request, res: Response) => {
+tempAccount.post("/jwtTempAccount", async (req: Request, res: Response) => {
+  const { firstName, lastName, birthDate, idCard, email } = req.body;
+  try {
+    await user.create({
+      firstName: firstName,
+      lastName: lastName,
+      birthDate: birthDate,
+      idCard: idCard,
+      email: email,
+    });
 
+    // dayjs.extend(isLeapYear) // use plugin
+    // dayjs.locale('zh-cn') // use locale
 
-    const { firstName, lastName, birthDate, idCard, email } = req.body;
-    try {
-        await user.create({
-            "firstName": firstName,
-            "lastName": lastName,
-            "birthDate": birthDate,
-            "idCard": idCard,
-            "email": email
-        })
+    let conunt = await user.countDocuments();
+    // console.log(conunt)
 
-        // dayjs.extend(isLeapYear) // use plugin
-        // dayjs.locale('zh-cn') // use locale
+    const myDate = dayjs();
+    // console.log(myDate.format('YYYYMMDD'));
 
-        let conunt = await user.countDocuments()
-        // console.log(conunt)
+    const password = await user.findOne({
+      idCard: idCard,
+    });
 
-        const myDate = dayjs();
-        // console.log(myDate.format('YYYYMMDD'));
+    const time = dayjs();
 
-        const password = await user.findOne({
-            idCard: idCard
-        })
+    let timeAdd = time.add(30, "minute");
+    // console.log(timeAdd)
 
-        const time = dayjs()
+    await auth.insertOne({
+      username: `TA${myDate.format("YYYYMMDD")}${conunt}`,
+      password: password?.idCard?.slice(7),
+      exp: `${myDate.format(`YYYY-MM-DD`)} ${timeAdd.format(`HH:mm:ss`)}`,
+    });
 
-        let timeAdd = time.add(30, 'minute')
-        // console.log(timeAdd)
+    let data = {
+      username: `TA${myDate.format("YYYYMMDD")}${conunt}`,
+      password: password?.idCard?.slice(7),
+      exp: `${myDate.format(`YYYY-MM-DD`)} ${timeAdd.format(`HH:mm:ss`)}`,
+    };
 
-        await auth.insertOne({
-            username: `TA${myDate.format('YYYYMMDD')}${conunt}`,
-            password: password?.idCard?.slice(7),
-            exp: `${myDate.format(`YYYY-MM-DD`)} ${timeAdd.format(`HH:mm:ss`)}`
-        })
+    let privateKey = "shhhh";
+    let token = jwt.sign({ data }, "shhhh", { expiresIn: "30m" });
 
-        res.status(200).send({
+    res.status(200).send({
+      status: 200,
+      msg: "success",
+      token,
+    });
+  } catch (error) {
+    res.status(500).send({
+      status: "OK",
+      msg: `Catch ${error}`,
+    });
+  }
+});
+
+tempAccount.post("/verify", async (req: Request, res: Response) => {
+  const { authorization }: any = req.headers;
+  console.log(authorization);
+  try {
+    jwt.verify(
+      authorization.split(" ")[1] || "",
+      "shhhh",
+      (err: any, user: any) => {
+        if (err) {
+          res.status(400).send({
+            status: 400,
+            msg: "Unauthor",
+          });
+        } else {
+          res.status(200).send({
             status: 200,
-            msg: "success",
-            data: {
-                username: `TA${myDate.format('YYYYMMDD')}${conunt}`,
-                password: password?.idCard?.slice(7),
-                exp: `${myDate.format(`YYYY-MM-DD`)} ${timeAdd.format(`HH:mm:ss`)}`
-            }
-        })
-    } catch (error) {
-        res.status(500).send({
-            status: "OK",
-            msg: `Catch ${error}`,
-        });
-    }
+            msg: "Success",
+            user
+          });
+        }
+      }
+    );
+  } catch (error) {
+    res.status(500).send({
+      status: "OK",
+      msg: `Catch ${error}`,
+    });
+  }
 });
